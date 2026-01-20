f09e676992219c56727e6a83a532c92d7ecb03b0
wellmiro <wellingtonmiranda23@yahoo.com>
2026-01-20 14:00:52 -0300

    fix: conexao render e ajuste de rotas cozinha

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-// Mantenha o link do ngrok como base enquanto estiver testando com o banco local
-// Quando mudar definitivamente para a AWS, basta trocar para o IP fixo
-const URL = 'https://nonautobiographical-uncapitulating-henry.ngrok-free.dev';
+// Agora a base é o seu domínio fixo no Render
+const URL = 'https://api-99burger.onrender.com';
 
 const api = axios.create({
     baseURL: URL,
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        // Esse header é o que libera o acesso no navegador e evita o erro 8012
-        'ngrok-skip-browser-warning': 'true' 
+        'Accept': 'application/json'
+        // Deletei o header do ngrok daqui
     }
 });
 
